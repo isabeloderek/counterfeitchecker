bc7bb1fe2da0e8e3f2dabd34521e226b551d1199
isabeloderek <derekisabelo07@gmail.com>
2024-04-29 17:50:04 +0800

    Added custom model

---
+++ app/src/main/java/com/example/counterfeitchecker/Constraints.java
@@ -0,0 +1,6 @@
+package com.example.counterfeitchecker;
+
+public class Constraints {
+    public static final String MODEL_PATH = "best_model.tflite";
+    public static final String LABELS_PATH = "labels.txt";
+}
